062bad5aac209b105cef880eed1670aa6082fdef
aryannsainiii-hub <aryannsainiii@gmail.com>
2026-03-10 09:36:56 +0530

    objects

---
+++ OneShot/Operators/operators.js
@@ -0,0 +1,34 @@
+let a=5;
+let b=2;
+
+console.log("a=",a,"&b=",b);
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+let age = 18;
+if(age>=18){
+    console.log("can votee");
+}
+if(age<18){
+    console.log("can't votee");
+}
+
+
+let num=10;
+if(num%2==0){
+    console.log("even");
+}
+else{
+    console.log("odd");
+}
+
+
+//ternary operator ---> (condition?true output:false output;)
+
+  alert("hello");
+let name=prompt("hello");
+console.log(name);
